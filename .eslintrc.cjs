2dfcbb9ed0a74d24f5646b860f5ac1e5f7cbcda4
BioMaRu <Bio.color.palette@gmail.com>
2023-07-31 21:55:53 +0700

    chore: update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,36 @@
 module.exports = {
 	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier',
+	],
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint', 'simple-import-sort'],
+	plugins: ['@typescript-eslint', 'simple-import-sort'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	rules: {
 		'simple-import-sort/imports': 'error',
-		'simple-import-sort/exports': 'error'
-	}
-};
+		'simple-import-sort/exports': 'error',
+		'@typescript-eslint/no-explicit-any': 'warn',
+	},
+}
